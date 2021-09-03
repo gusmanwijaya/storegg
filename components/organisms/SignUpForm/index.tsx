e73ef695a7fff8903ff250100314a834ee9b1a26
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-09-03 22:51:56 +0700

    Setup Sign Up Form

--- components/organisms/SignUpForm/index.tsx
+++ components/organisms/SignUpForm/index.tsx
@@ -1,6 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function SignUpForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const onSubmit = () => {
+    const userForm = {
+      email,
+      name,
+      password,
+    };
+
+    localStorage.setItem("user-form", JSON.stringify(userForm));
+    router.push("/sign-up-photo");
+  };
+
   return (
     <>
       <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign Up</h2>
@@ -19,6 +38,8 @@ export default function SignUpForm() {
           className="form-control rounded-pill text-lg"
           id="name"
           name="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
           aria-describedby="name"
           placeholder="Enter your name"
         />
@@ -35,6 +56,8 @@ export default function SignUpForm() {
           className="form-control rounded-pill text-lg"
           id="email"
           name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
           aria-describedby="email"
           placeholder="Enter your email address"
         />
@@ -51,19 +74,21 @@ export default function SignUpForm() {
           className="form-control rounded-pill text-lg"
           id="password"
           name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
           aria-describedby="password"
           placeholder="Your password"
         />
       </div>
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <Link href="/sign-up-photo">
-          <a
-            className="btn btn-sign-up fw-medium text-lg text-white rounded-pill mb-16"
-            role="button"
-          >
-            Continue
-          </a>
-        </Link>
+        <button
+          type="button"
+          onClick={onSubmit}
+          className="btn btn-sign-up fw-medium text-lg text-white rounded-pill mb-16"
+          role="button"
+        >
+          Continue
+        </button>
         <Link href="/sign-in">
           <a
             className="btn btn-sign-in fw-medium text-lg color-palette-1 rounded-pill"
