4492cf5929666da047f1f182d5cfa62c5113bc46
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-09-06 16:34:32 +0700

    Mapping data API ke Table Integration

--- components/organisms/TransactionContent/ButtonTab.tsx
+++ components/organisms/TransactionContent/ButtonTab.tsx
@@ -3,10 +3,11 @@ import classNames from "classnames";
 interface ButtonTabProps {
   title: string;
   active: boolean;
+  onClick: () => void;
 }
 
 export default function ButtonTab(props: ButtonTabProps) {
-  const { title, active } = props;
+  const { title, active, onClick } = props;
   const buttonClass = classNames({
     "btn btn-status rounded-pill text-sm me-3": true,
     "btn-active": active,
@@ -14,9 +15,9 @@ export default function ButtonTab(props: ButtonTabProps) {
 
   return (
     <>
-      <a data-filter="*" href="#" className={buttonClass}>
+      <button type="button" onClick={onClick} className={buttonClass}>
         {title}
-      </a>
+      </button>
     </>
   );
 }
--- components/organisms/TransactionContent/index.tsx
+++ components/organisms/TransactionContent/index.tsx
@@ -8,9 +8,10 @@ import TableRow from "./TableRow";
 export default function TransactionContent() {
   const [total, setTotal] = useState(0);
   const [transactions, setTransactions] = useState([]);
+  const [tab, setTab] = useState("all");
 
-  const getMemberTransactionAPI = useCallback(async () => {
-    const response = await getMemberTransactions();
+  const getMemberTransactionAPI = useCallback(async (value) => {
+    const response = await getMemberTransactions(value);
     if (response.error) {
       toast.error(response.message);
     } else {
@@ -21,10 +22,15 @@ export default function TransactionContent() {
   }, []);
 
   useEffect(() => {
-    getMemberTransactionAPI();
+    getMemberTransactionAPI("all");
   }, [getMemberTransactionAPI]);
 
   const IMG = process.env.NEXT_PUBLIC_IMG;
+
+  const onTabClick = (value) => {
+    setTab(value);
+    getMemberTransactionAPI(value);
+  };
 
   return (
     <>
@@ -48,10 +54,26 @@ export default function TransactionContent() {
           <div className="row mt-30 mb-20">
             <div className="col-lg-12 col-12 main-content">
               <div id="list_status_title">
-                <ButtonTab title="All Trx" active />
-                <ButtonTab title="Success" active={false} />
-                <ButtonTab title="Pending" active={false} />
-                <ButtonTab title="Failed" active={false} />
+                <ButtonTab
+                  onClick={() => onTabClick("all")}
+                  title="All Trx"
+                  active={tab === "all"}
+                />
+                <ButtonTab
+                  onClick={() => onTabClick("success")}
+                  title="Success"
+                  active={tab === "success"}
+                />
+                <ButtonTab
+                  onClick={() => onTabClick("pending")}
+                  title="Pending"
+                  active={tab === "pending"}
+                />
+                <ButtonTab
+                  onClick={() => onTabClick("failed")}
+                  title="Failed"
+                  active={tab === "failed"}
+                />
               </div>
             </div>
           </div>
